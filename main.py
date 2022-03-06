2167695c805d5f509bef3ffc56019662fcaca3d7
agiblida <agiblida@gmail.com>
2022-03-06 21:43:18 +0800

    esun setup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%%
 import os
 import random
 import numpy as np
@@ -5,13 +6,17 @@ import tensorflow as tf
 import importlib
 from data.dataset import Dataset
 from util import Configurator, tool
+import bottleneck as bn
+from tqdm import tqdm
+from util_esun.reco_utils import Evaluation
 
-
+#%%
 np.random.seed(2018)
 random.seed(2018)
 tf.set_random_seed(2017)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+#%%
 if __name__ == "__main__":
     conf = Configurator("NeuRec.properties", default_section="hyperparameters")
     gpu_id = str(conf["gpu_id"])
@@ -23,6 +28,9 @@ if __name__ == "__main__":
     # if Tool.get_available_gpus(gpu_id):
     #     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
     dataset = Dataset(conf)
+
+#%%
+if __name__ == "__main__":
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.gpu_options.per_process_gpu_memory_fraction = conf["gpu_mem"]
@@ -43,3 +51,30 @@ if __name__ == "__main__":
         model.build_graph()
         sess.run(tf.global_variables_initializer())
         model.train_model()
+        
+#%%
+# recommend
+if __name__ == "__main__":
+    to_cust_no = {v: k for k, v in dataset.userids.items()}
+    to_wm_prod_code = {v: k for k, v in dataset.itemids.items()}
+    
+    recommendation = {}
+    with tqdm(total=len(to_cust_no)) as pbar:
+        for k, v in to_cust_no.items():
+            pred_r = model.predict(k)
+            top_5_item_id = bn.argpartition(-pred_r, 5)[:5]
+            top_5_wm_prod_code = []
+            for i in range(len(top_5_item_id)):
+                top_5_wm_prod_code.append(to_wm_prod_code[top_5_item_id[i]])
+            recommendation[v] = top_5_item_id
+            pbar.update(1)
+        
+#%%
+# evaluate
+if __name__ == "__main__":
+    print('evaluating...')
+    evaluation = Evaluation('', './dataset_esun/2019-06-30/interaction_eval.csv', recommendation)
+    score_all = evaluation.results()
+    print("===", score_all)
+
+#%%
